b124cb573e833cdb9732315bdcbdb1158c026dfa
JulienAldon <julien2.aldon@epitech.eu>
2024-09-04 13:35:16 +0200

    Fix cleaness of code, bump vite and tauri versions

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -4,7 +4,7 @@ export default function Toggle(props) {
     return (
         <label title={props.description} className={styles.switchb}>
             <input id={props.login} onChange={(elem) => {props.onChange(elem.target.id)}} type="checkbox" checked={props.checked ? true : false}/>
-            <span class={styles.slider} />
+            <span className={styles.slider} />
         </label>
     );
 }
